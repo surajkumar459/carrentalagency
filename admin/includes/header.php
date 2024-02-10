d2c5f7920200438549ff2ed191dfa9a44c8a0a8d
SurajKumar <2411surajkumar@gmail.com>
2024-02-10 19:46:08 +0530

    first commit

---
+++ admin/includes/header.php
@@ -0,0 +1,14 @@
+<div class="brand clearfix">
+	<a href="dashboard.php" style="font-size: 20px; padding: 2rem;">Car Rental Agency | Admin Panel</a>  
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#">Admin Profile <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">Change Password</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
---
+++ db file/carrental.sql
@@ -0,0 +1,383 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 09, 2024 at 08:27 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `carrental`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2020-03-31 07:55:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbooking`
+--
+
+CREATE TABLE `tblbooking` (
+  `id` int(11) NOT NULL,
+  `userEmail` varchar(100) DEFAULT NULL,
+  `VehicleId` int(11) DEFAULT NULL,
+  `FromDate` varchar(20) DEFAULT NULL,
+  `ToDate` varchar(20) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `Status` int(11) DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblbooking`
+--
+
+INSERT INTO `tblbooking` (`id`, `userEmail`, `VehicleId`, `FromDate`, `ToDate`, `message`, `Status`, `PostingDate`) VALUES
+(1, 'samcurran123@gmail.com', 2, '05/02/2024', '25/02/2024', 'For Dehradun Visit ', 1, '2024-02-09 19:23:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbrands`
+--
+
+CREATE TABLE `tblbrands` (
+  `id` int(11) NOT NULL,
+  `BrandName` varchar(120) NOT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblbrands`
+--
+
+INSERT INTO `tblbrands` (`id`, `BrandName`, `CreationDate`, `UpdationDate`) VALUES
+(2, 'Maruti', '2024-02-09 16:20:22', NULL),
+(3, 'Toyota', '2024-02-09 18:18:51', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcontactusinfo`
+--
+
+CREATE TABLE `tblcontactusinfo` (
+  `id` int(11) NOT NULL,
+  `Address` tinytext DEFAULT NULL,
+  `EmailId` varchar(255) DEFAULT NULL,
+  `ContactNo` char(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblcontactusinfo`
+--
+
+INSERT INTO `tblcontactusinfo` (`id`, `Address`, `EmailId`, `ContactNo`) VALUES
+(1, 'New Asgard', 'john@gmail.com', '7525724672');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcontactusquery`
+--
+
+CREATE TABLE `tblcontactusquery` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `EmailId` varchar(120) DEFAULT NULL,
+  `ContactNumber` char(11) DEFAULT NULL,
+  `Message` longtext DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpages`
+--
+
+CREATE TABLE `tblpages` (
+  `id` int(11) NOT NULL,
+  `PageName` varchar(255) DEFAULT NULL,
+  `type` varchar(255) NOT NULL DEFAULT '',
+  `detail` longtext NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblpages`
+--
+
+INSERT INTO `tblpages` (`id`, `PageName`, `type`, `detail`) VALUES
+(1, 'Terms and Conditions', 'terms', ''),
+(2, 'Privacy Policy', 'privacy', ''),
+(3, 'About Us ', 'aboutus', '										<div>A car rental agency provides a convenient solution for individuals and businesses seeking temporary transportation. Whether it\'s for a weekend getaway, a business trip, or a special occasion, renting a car offers flexibility and freedom to explore without the commitment of ownership.&nbsp;<span style=\"font-size: 1em;\">These agencies typically offer a diverse fleet of vehicles to cater to various needs and preferences. From compact cars for fuel efficiency to spacious SUVs for family trips or luxury cars for a touch of sophistication, customers can choose the vehicle that best suits their requirements.</span></div><div><br></div><div>The process of renting a car is straightforward. Customers can either visit a physical rental location or make reservations online or over the phone. Rental terms are usually flexible, ranging from daily to weekly rentals. The rental agreement outlines the terms and conditions, including mileage limits, fuel policies, and insurance coverage.&nbsp;<span style=\"font-size: 1em;\">Car rental agencies prioritize customer satisfaction by ensuring the vehicles are well-maintained and undergo regular inspections. This commitment to quality and safety contributes to a positive rental experience.</span></div><div><br></div><div>One of the key advantages of renting a car is the convenience it offers in terms of transportation planning. Whether it\'s for a business trip where flexibility is crucial or a vacation where public transportation may be limited, having a rental car provides autonomy and convenience.&nbsp;<span style=\"font-size: 1em;\">Additionally, car rental agencies often offer loyalty programs, discounts, and promotional deals, making their services more attractive and cost-effective for frequent renters.</span></div><div><br></div><div>In summary, car rental agencies play a vital role in providing accessible and flexible transportation solutions. With a diverse fleet, easy reservation processes, and a commitment to customer satisfaction, these agencies contribute to making travel more convenient and enjoyable for individuals and businesses alike.</div>\r\n										'),
+(4, 'FAQs', 'faqs', 'Hello World');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsubscribers`
+--
+
+CREATE TABLE `tblsubscribers` (
+  `id` int(11) NOT NULL,
+  `SubscriberEmail` varchar(120) DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblsubscribers`
+--
+
+INSERT INTO `tblsubscribers` (`id`, `SubscriberEmail`, `PostingDate`) VALUES
+(1, 'gaurav41341@gmail.com', '2024-02-09 19:25:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbltestimonial`
+--
+
+CREATE TABLE `tbltestimonial` (
+  `id` int(11) NOT NULL,
+  `UserEmail` varchar(100) NOT NULL,
+  `Testimonial` mediumtext NOT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `id` int(11) NOT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `EmailId` varchar(100) DEFAULT NULL,
+  `Password` varchar(100) DEFAULT NULL,
+  `ContactNo` char(11) DEFAULT NULL,
+  `dob` varchar(100) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `City` varchar(100) DEFAULT NULL,
+  `Country` varchar(100) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblusers`
+--
+
+INSERT INTO `tblusers` (`id`, `FullName`, `EmailId`, `Password`, `ContactNo`, `dob`, `Address`, `City`, `Country`, `RegDate`, `UpdationDate`) VALUES
+(1, 'Sam Curran', 'samcurran123@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '9324313431', NULL, NULL, NULL, NULL, '2024-02-08 11:33:20', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblvehicles`
+--
+
+CREATE TABLE `tblvehicles` (
+  `id` int(11) NOT NULL,
+  `VehiclesTitle` varchar(150) DEFAULT NULL,
+  `VehiclesBrand` int(11) DEFAULT NULL,
+  `VehiclesOverview` longtext DEFAULT NULL,
+  `PricePerDay` float DEFAULT NULL,
+  `FuelType` varchar(100) DEFAULT NULL,
+  `ModelYear` int(6) DEFAULT NULL,
+  `SeatingCapacity` int(11) DEFAULT NULL,
+  `Vimage1` varchar(120) DEFAULT NULL,
+  `Vimage2` varchar(120) DEFAULT NULL,
+  `Vimage3` varchar(120) DEFAULT NULL,
+  `Vimage4` varchar(120) DEFAULT NULL,
+  `Vimage5` varchar(120) DEFAULT NULL,
+  `AirConditioner` int(11) DEFAULT NULL,
+  `PowerDoorLocks` int(11) DEFAULT NULL,
+  `AntiLockBrakingSystem` int(11) DEFAULT NULL,
+  `BrakeAssist` int(11) DEFAULT NULL,
+  `PowerSteering` int(11) DEFAULT NULL,
+  `DriverAirbag` int(11) DEFAULT NULL,
+  `PassengerAirbag` int(11) DEFAULT NULL,
+  `PowerWindows` int(11) DEFAULT NULL,
+  `CDPlayer` int(11) DEFAULT NULL,
+  `CentralLocking` int(11) DEFAULT NULL,
+  `CrashSensor` int(11) DEFAULT NULL,
+  `LeatherSeats` int(11) DEFAULT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblvehicles`
+--
+
+INSERT INTO `tblvehicles` (`id`, `VehiclesTitle`, `VehiclesBrand`, `VehiclesOverview`, `PricePerDay`, `FuelType`, `ModelYear`, `SeatingCapacity`, `Vimage1`, `Vimage2`, `Vimage3`, `Vimage4`, `Vimage5`, `AirConditioner`, `PowerDoorLocks`, `AntiLockBrakingSystem`, `BrakeAssist`, `PowerSteering`, `DriverAirbag`, `PassengerAirbag`, `PowerWindows`, `CDPlayer`, `CentralLocking`, `CrashSensor`, `LeatherSeats`, `RegDate`, `UpdationDate`) VALUES
+(1, 'Maruti Grand Vitara', 2, 'The highlight of the Maruti Suzuki Grand Vitara\'s exterior design is certainly how the headlamps, grille, and DRLs collectively make its nose look spectacular. Other highlights include the 16-inch dual-tone alloys, contrast-coloured skid plates, wraparound LED taillights, cladding, and the spoiler. It is available in nine colour shades, some offering a black roof option. \r\n\r\nInside the cabin, the Grand Vitara features a scenic panoramic sunroof, 360-degree camera, heads-up display, ventilated front seats, paddle shifters, and Suzuki Connect telematics. There\'s a barrage of safety features, too, such as six airbags, tyre pressure monitoring, hill hold assist, and hill descent control — to name a few. ', 2563, 'Diesel', 2023, 5, 'grand-vitara-exterior-right-front-three-quarter-4.webp', 'grand-vitara-exterior-right-side-view.webp', 'grand-vitara-interior-dashboard-2.webp', 'grand-vitara-exterior-left-front-three-quarter-39.webp', '', 1, 1, 1, 1, NULL, 1, 1, NULL, NULL, NULL, NULL, 1, '2024-02-09 16:31:27', NULL),
+(2, 'Toyota Innova Hycross', 3, 'The Hycross is a new model that has a more muscular appearance than the Crysta. It sports a large trapezoidal grille at the front, flanked by slim LED headlights. The front bumper features triangular vents with thin LED daytime running lights, while the rear receives wraparound taillights connected by a chrome strip.\r\n\r\nThe MPV\'s cabin gets a dual-tone interior with a choice of either seven or eight seats. It has a floating 10.1-inch touchscreen infotainment system, three-spoke steering wheel, semi-digital instrument cluster, gear shifter, electronic parking brake, and many other functions.', 3050, 'Petrol', 2021, 4, 'innova-hycross-exterior-right-front-three-quarter-73.webp', 'innova-hycross-exterior-front-view-3.webp', 'innova-hycross-exterior-right-front-three-quarter-72.webp', 'innova-hycross-exterior-right-rear-three-quarter.webp', 'innova-hycross-exterior-right-side-view.webp', 1, 1, NULL, NULL, NULL, 1, 1, 1, 1, NULL, NULL, NULL, '2024-02-09 18:23:59', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblbooking`
+--
+ALTER TABLE `tblbooking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblbrands`
+--
+ALTER TABLE `tblbrands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcontactusinfo`
+--
+ALTER TABLE `tblcontactusinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcontactusquery`
+--
+ALTER TABLE `tblcontactusquery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblpages`
+--
+ALTER TABLE `tblpages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblsubscribers`
+--
+ALTER TABLE `tblsubscribers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbltestimonial`
+--
+ALTER TABLE `tbltestimonial`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblvehicles`
+--
+ALTER TABLE `tblvehicles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblbooking`
+--
+ALTER TABLE `tblbooking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblbrands`
+--
+ALTER TABLE `tblbrands`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tblcontactusinfo`
+--
+ALTER TABLE `tblcontactusinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblcontactusquery`
+--
+ALTER TABLE `tblcontactusquery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tblpages`
+--
+ALTER TABLE `tblpages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `tblsubscribers`
+--
+ALTER TABLE `tblsubscribers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbltestimonial`
+--
+ALTER TABLE `tbltestimonial`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblvehicles`
+--
+ALTER TABLE `tblvehicles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
